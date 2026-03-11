6fda07bf50a627b162470672c29625aaa918d999
nikosappp <apostolopoulosn78@gmail.com>
2026-03-11 22:59:04 +0200

    sync full adder

---
+++ lab2/sync_full_adder.vhd
@@ -0,0 +1,27 @@
+-- NO PIPELINE --
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity sync_full_adder is
+    port (
+        clk : in std_logic;
+        a, b, cin: in std_logic;
+        sum, cout : out std_logic
+    );
+end sync_full_adder;
+
+architecture Behavioral of sync_full_adder is
+begin
+
+    process(clk)
+    begin
+    
+        if rising_edge(clk) then
+        
+            sum <= a xor b xor cin;
+            cout <= (a and b) or (a and cin) or (b and cin);
+         
+        end if; 
+    end process;
+end Behavioral;
